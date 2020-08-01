6c8b630c568547c2c3f5cfca6cd4eadad50f6222
Ross Perry <perryr16@gmail.com>
2020-08-01 12:57:43 -0600

    add simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.start
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -31,7 +33,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -61,7 +62,7 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")\
+  # config.filter_gems_from_backtrace("gem name")
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
@@ -69,5 +70,4 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
-
 end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -36,39 +36,13 @@ describe "Weather service" do
     lat = 39.738453
     lon = -104.984853
     body = service.get_weather(lat, lon)
-    
-    # now: name, temp, summary
-    expect(body[:hourly][0][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][0][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][0][:weather][0][:main].class).to be String
-    # +1 : name, temp, summary
-    expect(body[:hourly][1][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][1][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][1][:weather][0][:main].class).to be String
-    # +2 : name, temp, summary
-    expect(body[:hourly][2][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][2][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][2][:weather][0][:main].class).to be String
-    # +3 : name, temp, summary
-    expect(body[:hourly][3][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][3][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][3][:weather][0][:main].class).to be String
-    # +4 : name, temp, summary
-    expect(body[:hourly][4][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][4][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][4][:weather][0][:main].class).to be String
-    # +5 : name, temp, summary
-    expect(body[:hourly][5][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][5][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][5][:weather][0][:main].class).to be String
-    # +6 : name, temp, summary
-    expect(body[:hourly][6][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][6][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][6][:weather][0][:main].class).to be String
-    # +7 : name, temp, summary
-    expect(body[:hourly][7][:dt].is_a?(Numeric)).to be true
-    expect(body[:hourly][7][:temp].is_a?(Numeric)).to be true
-    expect(body[:hourly][7][:weather][0][:main].class).to be String
+
+    8.times do |index|
+      expect(body[:hourly][index][:dt].is_a?(Numeric)).to be true
+      expect(body[:hourly][index][:temp].is_a?(Numeric)).to be true
+      expect(body[:hourly][index][:weather][0][:main].class).to be String
+    end
+  
   end
 
   it "returns daily weather for today and 4 days out" do
@@ -77,37 +51,13 @@ describe "Weather service" do
     lon = -104.984853
     body = service.get_weather(lat, lon)
 
-    # today: name, summary, precip, high, low
-    expect(body[:daily][0][:dt].is_a?(Numeric)).to be true
-    expect(body[:daily][0][:rain].is_a?(Numeric)).to be true
-    expect(body[:daily][0][:temp][:max].is_a?(Numeric)).to be true
-    expect(body[:daily][0][:temp][:min].is_a?(Numeric)).to be true
-    expect(body[:daily][0][:weather][0][:main].class).to be String
-    # +1: name, precip, high, low, summary
-    expect(body[:daily][1][:dt].is_a?(Numeric)).to be true
-    expect(body[:daily][1][:rain].is_a?(Numeric)).to be true
-    expect(body[:daily][1][:temp][:max].is_a?(Numeric)).to be true
-    expect(body[:daily][1][:temp][:min].is_a?(Numeric)).to be true
-    expect(body[:daily][1][:weather][0][:main].class).to be String
-    # +2: name, precip, high, low, summary
-    expect(body[:daily][2][:dt].is_a?(Numeric)).to be true
-    expect(body[:daily][2][:rain].is_a?(Numeric)).to be true
-    expect(body[:daily][2][:temp][:max].is_a?(Numeric)).to be true
-    expect(body[:daily][2][:temp][:min].is_a?(Numeric)).to be true
-    expect(body[:daily][2][:weather][0][:main].class).to be String
-    # +3: name, precip, high, low, summary
-    expect(body[:daily][3][:dt].is_a?(Numeric)).to be true
-    expect(body[:daily][3][:rain].is_a?(Numeric)).to be true
-    expect(body[:daily][3][:temp][:max].is_a?(Numeric)).to be true
-    expect(body[:daily][3][:temp][:min].is_a?(Numeric)).to be true
-    expect(body[:daily][3][:weather][0][:main].class).to be String 
-    # +4: name, precip, high, low, summary
-    expect(body[:daily][4][:dt].is_a?(Numeric)).to be true
-    expect(body[:daily][4][:rain].is_a?(Numeric)).to be true
-    expect(body[:daily][4][:temp][:max].is_a?(Numeric)).to be true
-    expect(body[:daily][4][:temp][:min].is_a?(Numeric)).to be true
-    expect(body[:daily][4][:weather][0][:main].class).to be String
-    
+    5.times do |index|
+      expect(body[:daily][index][:dt].is_a?(Numeric)).to be true
+      expect(body[:daily][index][:rain].is_a?(Numeric)).to be true
+      expect(body[:daily][index][:temp][:max].is_a?(Numeric)).to be true
+      expect(body[:daily][index][:temp][:min].is_a?(Numeric)).to be true
+      expect(body[:daily][index][:weather][0][:main].class).to be String
+    end
   end
   
   

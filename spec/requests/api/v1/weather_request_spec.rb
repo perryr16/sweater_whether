b1a9dc666084f5adc6b9999e875d83c7469cf25b
Ross Perry <perryr16@gmail.com>
2020-08-01 13:47:26 -0600

    get forcast api functional

--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -5,7 +5,36 @@ describe "Weather API" do
   it "sends weather info for a location" do 
     get '/api/v1/forcast?location=denver,co'
 
-    binding.pry
+    expect(response.status).to eq(200)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body.keys).to eq([:forcast, :hourly, :daily])
+
+    exp_keys = [:id, :city, :state, :country, :high, :low, :date, :summary, :feels_like, :humidity, :visibility, :uv_index, :sunrise, :sunset, :created_at, :updated_at, :temp]
+    expect(body[:forcast].keys).to eq(exp_keys)
+    body[:forcast].each do |key, value|
+      expect(value.present?).to be true
+    end
+
+    exp_keys = [:id, :weather_id, :name, :summary, :temp, :index]
+    expect(body[:hourly].length).to eq(8)
+    body[:hourly].each do |hour|
+      expect(hour.keys).to eq(exp_keys)
+      hour.each do |key, value|
+        expect(value.present?).to be true 
+      end
+    end
+
+    exp_keys = [:id, :weather_id, :name, :summary, :precip, :high, :low, :index]
+    expect(body[:daily].length).to eq(5)
+    body[:daily].each do |hour|
+      expect(hour.keys).to eq(exp_keys)
+      hour.each do |key, value|
+        expect(value.present?).to be true 
+      end
+    end
+
   end
   
 end

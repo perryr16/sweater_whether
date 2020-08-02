9c708d766a7fe8d0b74f418318b70eded445891b
Ross Perry <perryr16@gmail.com>
2020-08-02 11:15:56 -0600

    added full state test to backgorund test

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -59,5 +59,30 @@ describe "Background API" do
     expect(body[:background5_url].include?('https://')).to be true
     expect(body[:background5_description].present?).to be true
   end
+
+  it "user enters a destination that is a state" do
+    
+    get '/api/v1/backgrounds?location=colorado'
+
+    expect(response.status).to eq(200)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+
+    expect(body[:background1_url].include?('https://')).to be true
+    expect(body[:background1_description].present?).to be true
+
+    expect(body[:background2_url].include?('https://')).to be true
+    expect(body[:background2_description].present?).to be true
+
+    expect(body[:background3_url].include?('https://')).to be true
+    expect(body[:background3_description].present?).to be true
+
+    expect(body[:background4_url].include?('https://')).to be true
+    expect(body[:background4_description].present?).to be true
+
+    expect(body[:background5_url].include?('https://')).to be true
+    expect(body[:background5_description].present?).to be true
+  end
   
 end

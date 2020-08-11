ab69610d45aff490273d707c2d56bcb2e84490e7
Ross Perry <perryr16@gmail.com>
2020-08-11 10:39:23 -0600

    formatting

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SessionsController < ApplicationController 
 
   def create
-
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
